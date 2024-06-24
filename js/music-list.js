c884d3675ebf697e3d831b0c88e3397a55b15057
Selflern-Pramee <honestqueen041@gmail.com>
2024-06-24 13:12:18 +0530

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,40 @@
+let allMusic = [
+
+    {
+        name: "Rooba Rooba ",
+        artist: "Chinmay , Shail Hada",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "O Range",
+        artist: "Benny Dayal",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Nenu Nuvvantu",
+        artist: "Naresh Iyer",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Chilipigaa",
+        artist: "Karthik",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Ola Olalaa Ala",
+        artist: "Ranina Reddy, N. C. Karunya",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Hello Rammante",
+        artist: "Vijay Prakash, Devan Ekambaram",
+        img: "music-6",
+        src: "music-6"
+    },
+
+];
